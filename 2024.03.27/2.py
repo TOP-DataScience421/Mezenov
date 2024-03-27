e62b37e1b516b081956819460929dd7f17a04ffb
Mikhail <mezenov_ms@mail.ru>
2024-03-27 23:20:11 +0300

    Считает предыд/след числа от введенного. Выводит в консоль

---
+++ 2024.03.27/2.py
@@ -0,0 +1,5 @@
+CurrNum = input('Введите целое число: ')
+NextNum = str(int(CurrNum)+1)
+PrevNum = str(int(CurrNum)-1)
+
+print('Следующее за числом', CurrNum,'число:',NextNum,'\nДля числа',CurrNum,'предыдущее число:',PrevNum)
