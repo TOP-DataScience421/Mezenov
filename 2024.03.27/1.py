885ad559c1fba587abd2fce80258aee3f5b71cb3
Mikhail <mezenov_ms@mail.ru>
2024-03-27 23:18:58 +0300

    Выводит введенные имя-фамилию. Считает текущий возраст(примерно)

---
+++ 2024.03.27/1.py
@@ -0,0 +1,6 @@
+Name = input('Введите Ваше имя: ')
+Surname = input('Введите Вашу фамилию: ')
+YearOfBirth = input('Введите год Вашего рождения: ')
+CurrentYear = 2024
+
+print('\nВаши данные: \n', Surname, Name+',', CurrentYear-int(YearOfBirth))
