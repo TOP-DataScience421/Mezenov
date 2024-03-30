5ae0201f13001c7ee58cfeffc79556dd0608b893
Mikhael <mezenov_ms@mail.ru>
2024-03-30 16:44:27 +0300

    Удалил еще одно ненужное преобразование объекта в тип str

--- 2024.03.27/2.py
+++ 2024.03.27/2.py
@@ -1,6 +1,6 @@
 CurrNum = input('Введите целое число: ')
-NextNum = str(int(CurrNum)+1)
-PrevNum = str(int(CurrNum)-1)
+NextNum = int(CurrNum)+1
+PrevNum = int(CurrNum)-1
 
 print('\nСледующее за числом', CurrNum, 'число:', NextNum, '\nДля числа', CurrNum, 'предыдущее число:', PrevNum)
 
